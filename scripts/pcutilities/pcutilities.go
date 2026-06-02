6a139080134b30e252f6c89f6c27036e005b2f62
Edgar-GIT <portalantigoeletronica@gmail.com>
2026-06-02 22:53:14 +0100

    Update pcutilities.go

--- scripts/pcutilities/pcutilities.go
+++ scripts/pcutilities/pcutilities.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"os"
 	"strings"
+	"time"
 
 	"programa/utils"
 )
@@ -15,7 +16,8 @@ func PCUtilitiesMenu() {
 		utils.ClearTerminal()
 		fmt.Printf("\n%s============ PC UTILITIES ============%s\n\n", utils.Blue, utils.Reset)
 		fmt.Printf("%s[1]  - System information (advanced + export)%s\n", utils.Green, utils.Reset)
-		utils.PrintReturnOption("2")
+		fmt.Printf("%s[2]  - Open reports folder%s\n", utils.Green, utils.Reset)
+		utils.PrintReturnOption("3")
 		fmt.Printf("\n%sOption: %s", utils.Green, utils.Reset)
 		line, err := reader.ReadString('\n')
 		line = strings.TrimSpace(line)
@@ -24,32 +26,82 @@ func PCUtilitiesMenu() {
 		}
 		switch line {
 		case "1":
-			utils.ClearTerminal()
-			r, err := CollectSystemReport()
-			if err != nil {
-				fmt.Printf("%s[!] %v%s\n", utils.Red, err, utils.Reset)
-				utils.WaitForEnter(reader)
-				continue
-			}
-			fmt.Print(RenderReportText(r, true))
-			fmt.Printf("\n%sExport report to target/pc_reports (.txt + .html)? [Y/n]: %s", utils.Yellow, utils.Reset)
-			ans, _ := reader.ReadString('\n')
-			ans = strings.ToLower(strings.TrimSpace(ans))
-			if ans == "" || ans == "y" || ans == "yes" {
-				tp, hp, ex := ExportReportFiles(r)
-				if ex != nil {
-					fmt.Printf("%s[!] Export failed: %v%s\n", utils.Red, ex, utils.Reset)
-				} else {
-					fmt.Printf("%sSaved:%s\n  %s\n  %s\n", utils.Green, utils.Reset, tp, hp)
-				}
-			}
-			fmt.Printf("\n%sPress Enter to continue...%s", utils.Green, utils.Reset)
-			reader.ReadString('\n')
+			runSystemReportFlow(reader)
 		case "2":
+			openReportsFolder(reader)
+		case "3":
 			return
 		default:
 			fmt.Printf("%sInvalid option.%s\n", utils.Yellow, utils.Reset)
 			utils.WaitForEnter(reader)
 		}
 	}
+}
+
+func runSystemReportFlow(reader *bufio.Reader) {
+	utils.ClearTerminal()
+	r, err := CollectSystemReport()
+	if err != nil {
+		fmt.Printf("%s[!] %v%s\n", utils.Red, err, utils.Reset)
+		utils.WaitForEnter(reader)
+		return
+	}
+
+	fmt.Print(RenderReportText(r, true))
+
+	fmt.Printf("\n%sExportar relatório para target/pc_reports (.txt + .html)? [Y/n]: %s", utils.Yellow, utils.Reset)
+	if !promptYesNo(reader, true) {
+		return
+	}
+
+	_, htmlPath, ex := ExportReportFiles(r)
+	if ex != nil {
+		fmt.Printf("%s[!] Export failed: %v%s\n", utils.Red, ex, utils.Reset)
+		utils.WaitForEnter(reader)
+		return
+	}
+
+	txtPath := strings.TrimSuffix(htmlPath, ".html") + ".txt"
+	fmt.Printf("%s[✓] Relatório guardado:%s\n", utils.Green, utils.Reset)
+	fmt.Printf("  %s\n  %s\n", txtPath, htmlPath)
+
+	fmt.Printf("\n%sAbrir relatório HTML no browser? [Y/n]: %s", utils.Yellow, utils.Reset)
+	if !promptYesNo(reader, true) {
+		return
+	}
+
+	if err := utils.OpenLocalHTML(htmlPath); err != nil {
+		fmt.Printf("%s[!] Não foi possível abrir no browser: %v%s\n", utils.Red, err, utils.Reset)
+		utils.WaitForEnter(reader)
+		return
+	}
+
+	fmt.Printf("%s[✓] Pedido enviado ao browser.%s\n", utils.Green, utils.Reset)
+	time.Sleep(800 * time.Millisecond)
+}
+
+func openReportsFolder(reader *bufio.Reader) {
+	dir := pathPCReportsDir()
+	if err := os.MkdirAll(dir, 0755); err != nil {
+		fmt.Printf("%s[!] %v%s\n", utils.Red, err, utils.Reset)
+		utils.WaitForEnter(reader)
+		return
+	}
+	if err := utils.OpenLocalFile(dir); err != nil {
+		fmt.Printf("%s[!] Não foi possível abrir a pasta: %v%s\n", utils.Red, err, utils.Reset)
+		fmt.Printf("%sCaminho: %s%s\n", utils.Yellow, dir, utils.Reset)
+		utils.WaitForEnter(reader)
+		return
+	}
+	fmt.Printf("%s[✓] Pasta aberta: %s%s\n", utils.Green, dir, utils.Reset)
+	time.Sleep(600 * time.Millisecond)
+}
+
+func promptYesNo(reader *bufio.Reader, defaultYes bool) bool {
+	ans, _ := reader.ReadString('\n')
+	ans = strings.ToLower(strings.TrimSpace(ans))
+	if ans == "" {
+		return defaultYes
+	}
+	return ans == "y" || ans == "yes" || ans == "s" || ans == "sim"
 }
