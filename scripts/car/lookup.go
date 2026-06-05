d2b5135e0716f3fbe5b4efdcdf646f23861a52d2
Edgar-GIT <portalantigoeletronica@gmail.com>
2026-06-05 14:08:49 +0100

    car

---
+++ scripts/car/lookup.go
@@ -0,0 +1,24 @@
+package car
+
+//web pages
+/*
+https://vehicleinfobyterabaap.vercel.app/lookup?rc=YOUR_PLATE_HERE
+https://infomatricula.pt/
+*/
+
+func default_search(){
+
+}
+
+func pt_plate(){
+
+}
+
+func printMenu(){
+
+}
+
+func main(){
+
+
+}
